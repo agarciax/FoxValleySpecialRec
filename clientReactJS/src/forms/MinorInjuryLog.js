2b74c6fea427b76d27be187d374a3ce4b47d8caa
Andy Garcia <75452731+GrandDukeX@users.noreply.github.com>
2022-02-22 11:38:53 -0600

    Minor Injury Log
    Minor Injury Log Fully Functional in React

--- clientReactJS/src/forms/MinorInjuryLog.js
+++ clientReactJS/src/forms/MinorInjuryLog.js
@@ -2,21 +2,50 @@ import "../css/MinorInjuryLogCSS.css"
 import "../images/icons/FVSRA_ICON.png"
 import logo from ".//PDRMA Form 03 Property Loss Report_files/Image_001.jpg"
 import {useState} from "react";
-
-// const [date, setDate] = useState('');
-// const [time, setTime] = useState('');
-  //const [injury, setInjury] = useState('');
-// const [injuryLocation, setInjuryLocation] = useState('');
-// const [treatment, setTreatment] = useState('');
-// const [injuryOccur, setInjuryOccur] = useState('');
-// const [facility, setFacility] = useState('');
-// const [staffName, setStaffName] = useState('');
+import {useNavigate} from "react-router-dom";
 
 
 const MinorInjury = () => {
-    return (  
+const [date, setDate] = useState('');
+const [time, setTime] = useState('');
+const [injury, setInjury] = useState('');
+const [injuryLocation, setInjuryLocation] = useState('');
+const [treatment, setTreatment] = useState('');
+const [injuryOccur, setInjuryOccur] = useState('');
+const [facility, setFacility] = useState('');
+const [staffName, setStaffName] = useState('');
+const navigate = useNavigate() //Like going back and forward in "history"/back from the previous or next page
+
+
+  const handleSubmit = (e) => { //Handles the onSubmit action of the log
+    e.preventDefault();
+
+    let minorInjuryLogObj = {
+      "injury_date": date,
+      "injury_time": time,
+      "name_of_injured": injury,
+      "injury_location": injuryLocation,
+      "treatment": treatment,
+      "how_injury_occurred": injuryOccur,
+      "facility_where_injury_occurred": facility,
+      "full_name_of_staff": staffName
+    }
+
+    fetch('http://127.0.0.1:5000/fvsra/minorInjuryLog', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(minorInjuryLogObj)
+    }).then(() => {
+      alert("Log has been reported.")
+      navigate('/login') //Redirects page
+    })
+
+  }
+
+  return (
 <div>
         <img className="MLF_FVSRA" src={logo} />
+  <form onSubmit={handleSubmit}>
         <table className="MLF_shift-down">
           <colgroup><col width={120} />
             <col width={110} />
@@ -26,7 +55,9 @@ const MinorInjury = () => {
             <col width={350} />
             <col width={200} />
             <col width={140} />
-          </colgroup><thead>
+          </colgroup>
+
+          <thead>
             <tr>
               <th>Date</th>
               <th>Time</th>
@@ -40,21 +71,22 @@ const MinorInjury = () => {
           </thead>
           <tbody>
             <tr>
-              <td><input type="date" name="date" id="date" /></td>
-              <td><input type="time" name="time" id="time" /></td>
-              <td><input type="text" placeholder="i.e Incision" name="injury" id="injury"  required /></td>
-              <td><input type="text" placeholder="i.e Right Leg" name="injury-location" id="injury-location" required /></td>
-              <td><input type="text" placeholder="i.e Bandage" name="treatment" id="treatment" required /></td>
-              <td><input type="text" placeholder="i.e Slipped on wet floor" name="injury-occur" id="injury-occur" required /></td>
-              <td><input type="text" placeholder="i.e Food Court" name="facility" id="facility" required /></td>
-              <td><input type="text" placeholder="John Doe" name="staff-name" id="staff-name" required /></td>
+              <td><input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} id="date" /></td>
+              <td><input type="time" name="time" value={time} onChange={(e) => setTime(e.target.value)} id="time" /></td>
+              <td><input type="text" placeholder="i.e Incision" name="injury" value={injury} onChange={(e) => setInjury(e.target.value)} id="injury"  required /></td>
+              <td><input type="text" placeholder="i.e Right Leg" name="injury-location" value={injuryLocation} onChange={(e) => setInjuryLocation(e.target.value)} id="injury-location" required /></td>
+              <td><input type="text" placeholder="i.e Bandage" name="treatment" value={treatment} onChange={(e) => setTreatment(e.target.value)} id="treatment" required /></td>
+              <td><input type="text" placeholder="i.e Slipped on wet floor" name="injury-occur" value={injuryOccur} onChange={(e) => setInjuryOccur(e.target.value)} id="injury-occur" required /></td>
+              <td><input type="text" placeholder="i.e Food Court" name="facility" value={facility} onChange={(e) => setFacility(e.target.value)} id="facility" required /></td>
+              <td><input type="text" placeholder="John Doe" name="staff-name" id="staff-name" value={staffName} onChange={(e) => setStaffName(e.target.value)} required /></td>
             </tr>
           </tbody>
         </table>
       
           <footer className="MLF_bottom">
-          <button className="MLF_submit" onclick="submitBtn()">Submit</button>
+          <button className="MLF_submit">Submit</button>
         </footer>
+  </form>
         </div>
     );
 }
